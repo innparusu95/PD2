10ebfe1df239b8d081482c5b8e5086352411f715
innparusu <e125716@gmail.com>
2013-05-21 18:19:08 +0900

    Accountクラスのカウント条件を変更

--- Account.rb
+++ Account.rb
@@ -1,9 +1,10 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 require 'tweetstream'
 require 'Twitter'
 
 class Account
-  def initialize(consumer_key,consumer_secret,oauth_token,oauth_token_secret)
+
+  def initialize consumer_key,consumer_secret,oauth_token,oauth_token_secret
     #ログイン
     Twitter.configure do |config|
       config.consumer_key = consumer_key 
@@ -22,25 +23,25 @@ class Account
 
     #インスタンスフィールド
     @USER_NAME = Twitter.user().screen_name
-    @MATCH_NAME = Regexp.new("#{@USER_NAME} .*")
+    @MATCH_NAME = Regexp.new("@#{@USER_NAME} .*")
     @client = TweetStream::Client.new
   end
 
-  def userStream()
+  def userStream
     setMentions
     @client.userstream do |status|
       addMentions(status)
     end
   end
 
-  def addMentions(status)
+  def addMentions status
     if @MATCH_NAME =~ status.text
       puts "#{status.user.screen_name}:#{status.text}"
       setMentions
     end
   end
 
-  def setMentions()
+  def setMentions
     @hash = Hash.new(0)
     Twitter.mentions.each do |tweet| 
       @hash[tweet.user.screen_name] += 1 
